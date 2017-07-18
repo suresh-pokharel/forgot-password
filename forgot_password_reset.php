feb4542825fbfd8c5eb7157f4a6862aad8141a95
Suresh Pokharel <suresh.wrc@gmail.com>
2017-07-18 12:02:06 +0545

    Update forgot_password_reset.php

--- forgot_password_reset.php
+++ forgot_password_reset.php
@@ -1,3 +1,12 @@
+<?php
+/*
+      Author  : Suresh Pokharel
+      Email   : suresh.wrc@gmail.com
+      GitHub  : github.com/suresh021
+      URL     : psuresh.com.np
+*/ 
+?>
+
 <?php 
 $message="";
 $valid='true';
